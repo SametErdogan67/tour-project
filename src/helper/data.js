23aa2db608e8f0080e38f5a22559b776e852fad6
SametErdogan67 <samedi6774@gmail.com>
2023-04-23 22:03:12 +0300

    karadeniz tour guide

--- src/helper/data.js
+++ src/helper/data.js
@@ -1,82 +1,84 @@
 export const data = [
   {
     id: 1,
-    title: "New York",
-    desc: "Home to the Statue of Liberty, Empire State Building, Central Park, Times Square, New York is the most populous city in the USA. We have a lot to tell about the city where city lights illuminate the sky.",
+    title: "Trabzon, Uzungöl",
+    desc: "Çaykara ilçesinde bulunan Uzungöl Mahallesi, ismini sınırları içerisinde bulunan heyelan set gölünden almıştır. Deniz seviyesinden ortalama 1000 metre yükseklikte olan bu mahallesinden doğa yürüyüşü yaparak ünlü Haldizen Yaylası’na ulaşılmaktadır. Hem yürüyüş yolu hem de Uzungöl’ün çevresi kuş gözlemciliği yapılabilecek en güzel yerlerdendir.",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/07/15/47/new-york-city-2380683_960_720.jpg",
+      "https://www.rentgo.com/wp-content/uploads/2021/05/trabzon-uzungol-768x432.jpg",
   },
   {
     id: 2,
-    title: "Philippines",
-    desc: "The island country, the Philippines, is one of the most beautiful countries where blue and green are together. Forget the potion of youth, traveling to the Philippines will rejuvenate you for at least 5 years.",
+    title: "Trabzon, Sümela Manastırı",
+    desc: "Sümela Manastırı, yakından bakıldığı taktirde oldukça etkileyici olmaktadır. Zira bu manastır çok dik bir yamaca, insanların dahi zor yürüyebileceği bir bölgeye yapılmıştır. Manastırın denizden yüksekliği yaklaşık 1150 metredir. Rum Ortodoks Manastırı Kilisesi olan bu tarihi yapı ‘’Dünya Kültür Mirası Listesi’’ içerisinde yer almıştır. Manastırın yapılış tarihi 13. yüzyıl olarak bilinmektedir ve yıllar içerisinde sürekli bir yenilenme durumu söz konusu olmuştur. Yüzlerce yıl faaliyette kalmış olan manastır içerisinde zamanla yapılan restore çalışmalarıyla, cennet ve cehennem, ölüm ve yaşam imgeleri taşıyan freskler bulunmuştur. Bunun yanı sıra manastır içerisine gizli bir geçitle ulaşılan şapel, gözetleme yeri ve çile odası da bulunmaktadır.",
     image:
-      "https://i.natgeofe.com/n/04505c35-858b-4e95-a1a7-d72e5418b7fc/steep-karst-cliffs-of-el-nido-in-palawan.jpg?w=2880&h=1440",
+      "https://www.rentgo.com/wp-content/uploads/2021/05/trabzon-sumele-manastiri.jpg",
   },
   {
     id: 3,
-    title: "Bali",
-    desc: "Bali, Indonesia's most famous island, has taken its place as the 'last paradise' in mind. Providing 80% of its livelihood with tourism, Bali hosts thousands of tourists every year.",
-    image: "https://ak.picdn.net/shutterstock/videos/26368511/thumb/1.jpg",
+    title: "Rize, Ayder Yaylası",
+    desc: "Ayder Yaylası’nda sayısız doğal güzellik yer almaktadır. Yaylanın en dikkat çekici özelliklerinden birisi kaplıcalarıdır. Yayladan çıkan dağ suyunun birçok hastalığa iyi geldiği söylenmektedir. Ayrıca yayladaki görülmesi gereken yapılardan en önemlisi 750 metre rakıma sahip olan Zil Kalesi’dir. Yayladaki köy evlerinde konaklama imkanı da bulunmaktadır.",
+    image:
+      "https://www.rentgo.com/wp-content/uploads/2021/05/ayder-yaylasi-768x511-min.jpg",
   },
   {
     id: 4,
-    title: "Egypt",
-    desc: "Being one of the biggest countries of the Middle East, Egypt has been the transition point of many civilizations with its thousands of years of history. The pyramids and miracles that come to mind when talking about Egypt have been the subject of many films. It still remains a mystery.",
-    image:
-      "https://images.memphistours.com/large/34d5b5a3fbaa4b3b5d9487bf924b0145.jpg",
+    title: "Amasya",
+    desc: "Karadeniz gezi rehberi denildiği zaman, elbette yaylalar ve tarihi yapıtlar ile sınırlı kalmak olmaz. Amasya, doğal güzelliği, geçmişi ve tarihi eserleriyle oldukça zengin bir turistik yapıya sahiptir.",
+    image: "https://www.rentgo.com/wp-content/uploads/2021/06/amasya.jpg",
   },
   {
     id: 5,
-    title: "Cappadocia",
-    desc: "Cappadocia… 'Kappa Tuchia', which means 'Land of Beautiful Horses' in the words of Persians. A location in Turkey and in the center of Anatolia's most popular and unique tourist centers of someone who Cappadocia, magnificent nature, valleys of unique beauty, fairy chimneys, rock-hewn settlements, millions every year with open-air museums and underground cities, local and hosts foreign tourists.",
-    image: "https://i.hurimg.com/i/hdn/75/0x0/5aa10544d3806c25541af273.jpg",
+    title: "Samsun, Şahinkaya Kanyonu",
+    desc: "Şahinkaya Kanyonu, Kızılırmak üzerindeki en dar ve en uzun geçit olarak bilinmektedir. 2,5 kilometre uzunluğuna sahip olan kanyon kayalıklarının denize olan mesafesi ise 324 metredir. Kanyonun oluşumunun ise, Kızılırmak’ı yarması sonucunda olduğu bilinmektedir.",
+    image:
+      "https://www.rentgo.com/wp-content/uploads/2021/06/samsun-sahinkaya-kanyonu.jpg",
   },
   {
     id: 6,
-    title: "Paris",
-    desc: "Paris has been recognized as one of the most romantic cities in the world. Many famous buildings here have been added to the Unesco World Heritage List. It awaits you with its museums, fashion and historical buildings.",
+    title: "Bolu, Abant Gölü",
+    desc: "Batı Karadeniz’in en güzel göllerinden birisi şüphesiz Abant Gölü’dür. Mudurnu ilçesine 18 kilometre mesafede bulunan Abant Gölü, yaklaşık 1.350 metre yüksekliktedir. Bölge, tabiat parkı olarak 1988’de koruma altına alınmıştır. Çeşit çeşit ağaçların ve bitkilerin bulunduğu Abant Gölü ve çevresi harika tabiatı ile ziyaretçilerini oldukça etkilemektedir. Bunun yanı sıra, sadece bölgeye özgü olan ve Abant Gölü’nün yüksek yerlerinde bulunan Abant Çiğdemi ile göl içinde bulunan Abant Alabalığı da bölgenin endemik türlerinden olmaktadır. Abant Gölü’nü, Batı Karadeniz gezilecek yerler listenize mutlaka dahil edin.",
     image:
-      "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180221130827/iStock-155381519.jpg",
+      "https://www.rentgo.com/wp-content/uploads/2021/05/bolu-abant-golu-min.jpg",
   },
   {
     id: 7,
-    title: "Venice",
-    desc: "Venice, one of Italy's indispensable stops, is known as the 'City of Channels', 'City of Waters' or 'City of Masks' thanks to its world-famous masks. If you visit in February, you can catch the Venice Carnival, where everyone wears a mask and has fun.",
+    title: "Bolu, Yedigöller Milli Parkı",
+    desc: "Tabiatının güzelliği ve bitki örtüsünün zenginliğiyle bilinen Yedigöller, Mengen ve Yığılca sınırlarında bulunmaktadır. Yedigöller, 1965 yılında milli park olarak ilan edilmiş ve o dönemden günümüze kadar her sene binlerce turisti misafir etmiştir.",
     image:
-      "https://www.qantas.com/content/travelinsider/en/explore/europe/italy/venice/things-you-need-to-know-before-you-go-to-venice/_jcr_content/parsysTop/hero.img.full.medium.jpg/1535006246068.jpg",
+      "https://www.rentgo.com/wp-content/uploads/2021/06/bolu-yed%C2%A6-goller-m%C2%A6-illi-parki.jpg",
   },
   {
     id: 8,
-    title: "India",
-    desc: "If you are not a vacationer but a traveler, we can say that India is the place for you. India, one of the most colorful places in the world, has a crazy variety. The Taj Mahal, the story of which you have heard more or less, is one of its famous buildings.",
-    image: "https://lp-cms-production.imgix.net/2020-11/GettyRF_494057771.jpg",
+    title: "Zonguldak, Filyos Kalesi",
+    desc: "Tieion Antik Kenti olarak bilinen yerde bulunan Filyos Kalesi’nin, Eski Roma döneminde inşa edildiği düşünülmektedir. Kale, Karadeniz maviliğine doğru bulunmakta ve buradaki bir burun üzerinde durmaktadır. Kalenin inşasında oldukça büyük ve sağlam taşlar kullanıldığı bilinmektedir. Görkemli görüntüsü ile ve tarihi yapısıyla her yıl çok sayıda ziyaretçiye ev sahipliği yapmaktadır",
+    image:
+      "https://www.rentgo.com/wp-content/uploads/2021/05/filyos-kalesi-min.jpg",
   },
   {
     id: 9,
-    title: "Northern Lights",
-    desc: "Northern lights, the 'Aurora Borealis', named after the Dawn Goddess Aurora, are the natural glows that emerge in the sky with the interaction of the earth's magnetic field and charged particles from the Sun. This image, which makes most of us feel like in a fairy tale land, deserves to be visited at least once in a lifetime.",
-    image:
-      "https://images.unsplash.com/photo-1579033461380-adb47c3eb938?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bm9ydGhlcm4lMjBsaWdodHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    title: "Bartın, Amasra",
+    desc: "Karadeniz’in doğası yalnızca yeşilliği ile ön plana çıkmamaktır. Karadeniz yaylaları ve şelalelerinin yanı sıra, bazı şehirleri vardır ki, oraya gider gitmez bir ruha sahip olduğu hissedilebilir. Bu şehirlerden biri Bartın’dır.",
+    image: "https://www.rentgo.com/wp-content/uploads/2021/05/bartin-min.jpg",
   },
   {
     id: 10,
-    title: "Fuji Mountain",
-    desc: "The symbol and highest mountain of Japan, Mount Fuji is considered sacred by the Japanese and is called Fuji-San. This mountain, which has an active startovolcano, is depicted in art and photography and is a natural wonder visited by athletes and tourists.",
+    title: "Artvin, Maral Şelalesi",
+    desc: "Maral Şelalesi, Maralköy içerisinde bulunan ve oldukça görkemli bir görünüşe sahip olan meşhur şelaledir. Şelaleye ulaşmak için, yaklaşık 15 dakika kadar yürümeniz gerekir ve yüzmek için de oldukça uygun bir yerdir. Gerek çevresinde bulunan tabiatın güzelliğiyle gerekse canlandırıcı soğuk suları ile ziyaretçileri oldukça cezbetmektedir. ",
     image:
-      "https://d20aeo683mqd6t.cloudfront.net/articles/title_images/000/038/943/original/pixta_43064884_S.jpg?2019&d=750x400",
+      "https://trthaberstatic.cdn.wp.trt.com.tr/resimler/1936000/selale-iha-1937277.jpg",
   },
   {
     id: 11,
-    title: "Arashiyama Bamboo Grove",
-    desc: "Bamboo Forest, or Arashiyama Bamboo Grove or Sagano Bamboo Forest, is a natural forest of bamboo in Arashiyama, Kyoto, Japan. The forest consists mostly of Moso Bamboo (Phyllostachys edulis) and consists of several pathways for tourists and visitors. The Ministry of the Environment considers it a part of the soundscape of Japan.",
-    image: "https://wallpaperaccess.com/full/534182.jpg",
+    title: "Rize, Anzer Yaylası",
+    desc: "Rize’nin İkizdere ilçesine bağlı olan ve yaklaşık 35 km uzaklıkta bulunan Anzer Yaylası gerek tabiatı ile gerekse temiz havası ile her sene çok sayıda turisti misafir etmektedir. Endemik türde çiçeklerin bulunduğu bu yayla, Türkiye coğrafyasındaki en önemli yerleşkelerden biridir. Anzer Yaylası, doğası ve havasıyla olduğu kadar aynı zamanda balı ile de meşhur bir yayladır. Rize’de ne yapılır, sorusuna verilecek en güzel ve en sağlıklı yanıtlardan birisi Anzer balı yemek olacaktır.",
+    image:
+      "https://www.rentgo.com/wp-content/uploads/2021/05/anzer-yaylasi-min.jpg",
   },
   {
     id: 12,
-    title: "Antelope Canyon",
-    desc: "Antelope Canyon is a slot canyon in the American Southwest, on Navajo land east of Page, Arizona. It includes two separate, scenic slot canyon sections, referred to as Upper Antelope Canyon (or The Crack), and Lower Antelope Canyon (or The Corkscrew).",
+    title: "Gümüşhane, Tomara Şelalesi",
+    desc: "Tomara Şelalesi sahip olduğu ismi bölgede yetişen ve yılda bir kez olan Tomara bitkisinden almıştır. Görkemli bir şekilde akan suyu ve şelalenin çıkardığı doğal köpük sesi ziyaretçiler için oldukça dinlendirici bir etki yaratır. Şelale yaklaşık olarak 25 metre yükseklikten akmaktadır ve şelalenin tepesinden enfes Gümüşhane şehir manzarası görülür.",
     image:
-      "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxvd2VyJTIwYW50ZWxvcGUlMjBjYW55b258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://www.rentgo.com/wp-content/uploads/2021/06/gumushane-tomara-selalesi.jpg",
   },
 ];
